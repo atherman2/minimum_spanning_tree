c3375c95d43f70ddc216225a38a37304e110b3f3
atherman2 <alexandre.t.herman@gmail.com>
2025-12-10 13:46:48 -0300

    definitions usage example in main file

--- src/main.go
+++ src/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	mu "minimum_spanning_tree/mst_util"
+	st "minimum_spanning_tree/structure"
 )
 
 func main() {
 	fmt.Println("this is minimum_spanning_tree source code main file")
+	var v_list st.Vertex_list = st.Vertex_linked_list{V: st.Vertex(1), Next: nil, Weight: st.Weight(1)}
+	fmt.Println(mu.Search(v_list, st.Vertex(1)))
+	fmt.Println(mu.Search(v_list, st.Vertex(2)))
 }
